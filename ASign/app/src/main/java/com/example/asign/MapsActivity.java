53bec33f56062dd369bd8c2c90b08ff4b49ea928
Mina Nashed <mmnashed@myseneca.ca>
2019-10-25 19:25:26 -0400

    Test Commit
    - Testing commits and pushes
    - printing test statment to console

--- ASign/app/src/main/java/com/example/asign/MapsActivity.java
+++ ASign/app/src/main/java/com/example/asign/MapsActivity.java
@@ -21,6 +21,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+
+        System.out.println("TEST TEST FADY LET ME KNOW IF U SEE THIS COMMIT/PUSH");
+        
         mapFragment.getMapAsync(this);
     }
 
