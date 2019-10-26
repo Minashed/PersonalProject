02d6f03228da66e1b8dce76f3bf75df63fa496f1
Mina Nashed <mmnashed@myseneca.ca>
2019-10-25 21:11:22 -0400

    Finally Got Mpas to Work
     - Map showing on main actvity a random location specified in code

--- ASign/app/src/main/java/com/example/asign/MainActivity.java
+++ ASign/app/src/main/java/com/example/asign/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.asign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        map  = googleMap;
+
+        LatLng randomLocation = new LatLng(43.903908, -79.391056);
+        map.addMarker(new MarkerOptions().position(randomLocation).title("RANDOM LOCATION"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(randomLocation));
+
     }
 }
